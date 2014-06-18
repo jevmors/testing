642d2b79bb55a5b611b4b357373a88b5b00a8abd
Robin Xiang <jevmors@gmail.com>
2014-06-18 16:34:16 -0700

    123

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -14,6 +14,7 @@ public class Main {
 		System.out.println("Hello World3!");
 		System.out.println("Hello World4!");
 		System.out.println("Hello World5!");
+		System.out.println("Hello World120");
 	}
 
 }
