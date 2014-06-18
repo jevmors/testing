d569443670cc9302ea3b43fc62259d6ee63ec9d5
Robin Xiang <jevmors@gmail.com>
2014-06-18 11:47:39 -0700

    11

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -10,10 +10,12 @@ public class Main {
 	public static void displayGreeting(){
 		System.out.println("Hello World!");
 		System.out.println("Hello World2!");
+
 		System.out.println("Hello World3!");
 		System.out.println("Hello World4!");
 		System.out.println("Hello World5!");
 		System.out.println("Hello World6!");
+
 
 	}
 
